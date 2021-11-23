9af743a9e1f80c41ccc8c54d93d4216faf38b016
Zacharie BARBECOT <z.barbecot@gmail.com>
2021-11-23 12:22:11 +0100

    write acceptance test

--- src/test/java/acceptance/PokerGameTest.java
+++ src/test/java/acceptance/PokerGameTest.java
@@ -55,4 +55,37 @@ public class PokerGameTest {
         assertThat(result)
                 .isEqualTo(expected);
     }
+
+    @Test
+    void should_have_winner_black_with_pair_of_king() {
+        String input = "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH";
+        Player black = new Player(
+                "Black",
+                new StartingHand(
+                        new Card(Rank.TWO, Suit.HEARTS),
+                        new Card(Rank.TWO, Suit.DIAMONDS),
+                        new Card(Rank.KING, Suit.SPADES),
+                        new Card(Rank.NINE, Suit.CLUBS),
+                        new Card(Rank.KING, Suit.DIAMONDS)
+                )
+        );
+        Player white = new Player(
+                "White",
+                new StartingHand(
+                        new Card(Rank.FIVE, Suit.CLUBS),
+                        new Card(Rank.FIVE, Suit.HEARTS),
+                        new Card(Rank.FOUR, Suit.SPADES),
+                        new Card(Rank.EIGHT, Suit.CLUBS),
+                        new Card(Rank.ACE, Suit.HEARTS)
+                )
+        );
+        String expected = "Black wins. - with pair of: King";
+        when(parser.parse(input))
+                .thenReturn(List.of(black, white));
+
+        String result = game.result(input);
+
+        assertThat(result)
+                .isEqualTo(expected);
+    }
 }
